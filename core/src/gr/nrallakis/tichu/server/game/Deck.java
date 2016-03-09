b4db40cbe0bb2e1ea9d1446ab622f5bc57facab4
nrallakis <nrallakis4@gmail.com>
2016-03-09 19:48:45 +0200

    Added drawSixCards which returns Card[]

--- core/src/gr/nrallakis/tichu/server/game/Deck.java
+++ core/src/gr/nrallakis/tichu/server/game/Deck.java
@@ -1,29 +1,48 @@
 package gr.nrallakis.tichu.server.game;
 
-import com.badlogic.gdx.utils.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Deck {
 
-    private Array<Card> cards;
+    private List<Card> cards;
 
     public Deck() {
-        cards = new Array<Card>(56);
+        cards = new ArrayList<>(56);
+        addNormalCards();
+        addSpecialCards();
+        shuffle();
+    }
 
+    private void addNormalCards() {
         for (int type = 0; type < 3; type++) {
             for (int rank = 2; rank < 14; rank++) {
                 cards.add(new Card(type, rank));
             }
         }
-        //Add the special cards
-        cards.add(new Card(Card.DOGS));
-        cards.add(new Card(Card.MAHJONG));
-        cards.add(new Card(Card.PHOENIX));
-        cards.add(new Card(Card.DRAGON));
+    }
 
-        cards.shuffle();
+    private void addSpecialCards() {
+        cards.add(new Card(Card.DOGS, 0));
+        cards.add(new Card(Card.MAHJONG, 1));
+        cards.add(new Card(Card.PHOENIX, -1));
+        cards.add(new Card(Card.DRAGON, 25));
+    }
+
+    private void shuffle() {
+        Collections.shuffle(cards);
     }
 
     public Card drawCard() {
-        return cards.removeIndex(0);
+        return cards.remove(0);
+    }
+
+    public Card[] drawSixCards() {
+        Card[] sixCards = new Card[6];
+        for (int i = 0; i < sixCards.length; i++) {
+            sixCards[i] = drawCard();
+        }
+        return sixCards;
     }
 }
